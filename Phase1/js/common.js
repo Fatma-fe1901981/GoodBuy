f9a92005a5df9caba06fc86622942629b7d0d4c4
Fatma Ali Elbakry Elbadrawy <fe1901981@qu.edu.qa>
2024-03-29 02:00:39 +0300

    some navigation css, js, and seller form

---
+++ Phase1/js/common.js
@@ -0,0 +1,47 @@
+const nav = document.querySelector('#nav');
+const footerSection = document.querySelector('#footer');
+
+document.addEventListener('DOMContentLoaded', async () => {
+    try {
+        showMenu();
+        footer();
+        // other functions????
+    } catch (error) {
+        console.error("Failed to load books:", error);
+    }
+});
+
+function showMenu() {
+    nav.innerHTML = `
+    <div class="nav-container">
+        <div class="nav-container">
+        <div class="logo">
+            <img src="/images/logo.png" alt="logo image">
+            <p>GoodBuy</p>
+        </div> 
+
+        <div class="links">
+            <nav class="nav-links" id="nav-links">
+                <a href="/index.html" class="hamburger-menu">Home</a>
+                <a href="/profile.html">Profile</a>
+                <a href="/login.html">Login</a>
+            </nav>
+            <a href="javascript:void(0);" class="hamburger-menu" onclick="menuDisplay()">
+                <i class="fa fa-bars"></i>
+            </a>
+        </div>
+    </div>`;
+}
+
+function footer() {
+    footerSection.innerHTML = `<p>&copy; | Goodbuy 2024 | CMPS350 Couse Project | G3 L51</p>`;
+}
+
+function menuDisplay() {
+    let element = document.getElementById("nav-links");
+    if (element.style.display === "block") {
+        element.style.display = "none";
+    } else {
+        element.style.display = "block";
+    }
+}

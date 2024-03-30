011944470c1f0f41468bc173ffa725cb8a9a9d27
bshayer <124089539+bshayer-ba2007129@users.noreply.github.com>
2024-03-31 01:03:07 +0300

    use case 4 js final

--- Phase1/js/history.js
+++ Phase1/js/history.js
@@ -1,12 +1,27 @@
 
 const userHistory = document.querySelector('#user-info');
+const sellingInfo=document.querySelector('#selling-div')
+const soldInfo=document.querySelector('#sold-div')
 
 document.addEventListener('DOMContentLoaded', async () => {
+ 
     try {
-        let customer = JSON.parse(localStorage.currentUser)
+         const user = JSON.parse(localStorage.currentUser)
+        if(user !== "undefined")  
+        {  if (user.role==="customer") {
+            console.log("im customer");
+               showHistoryCustomer();
+             
+            }else if(user.role==="seller"){  
+                showHistoryseller();
+                console.log("im seller");
+                document.getElementById("btn-add").style.display= "block";
 
-        if(customer !== 'undefined')   
-            showHistory()
+             }
+        } else {
+            console.log("you are an admin");
+        }
+            
         // other functions????   
     } catch (error) {
         alert("you need to log in")
@@ -14,8 +29,10 @@ document.addEventListener('DOMContentLoaded', async () => {
 });
 
 let purchasedItemsList = []
+let sellingList=[]
+let SoldList=[]
 
-function showHistory() {
+function showHistoryCustomer() {
     const customer = JSON.parse(localStorage.currentUser)
     console.log(customer);
     purchasedItemsList = customer.purchasedItems
@@ -25,16 +42,32 @@ function showHistory() {
     userHistory.innerHTML = htmlProducts
 }
 
+function showHistoryseller(){
+    document.getElementById("soldInfo").style.display= "block";
+    document.getElementById("selling-products").style.display= "block";
+    const seller = JSON.parse(localStorage.currentUser)
+   sellingList=seller.itemsBeingSold
+   SoldList =seller.itemsSold
+    const htmlSold = SoldList.map(product1 => productHtml(product1)).join(" ")
+    const htmlSelling=sellingList.map(product1 => productHtml(product1)).join(" ")
+    sellingInfo.innerHTML=htmlSelling
+    soldInfo.innerHTML= htmlSold
+}
+
+
 function productHtml(product) {
     return ` 
         <img src="${product.product_image}" alt="product image">
         <div class="productName-fav">
             <p>${product.product_name}</p>
-            <button type="button" id="heart"><i class="fa-regular fa-heart"></i></button>
         </div>
         <div class="price-purchase">
             <p>QR <span>${product.product_price}</span></p>
-            <a id="link" href="/sub-pages/cart.html" onclick="purchaseItem(${product.id})">More Information</a>
+        </div>
+        <div class="quantity">
+        <p>quantity: <span>${product.product_quantity}</span></p>
         </div>
     `
 }
+
+

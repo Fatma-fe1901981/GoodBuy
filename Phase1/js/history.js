34c32c3449bd814eb745b21d4b72bd7a06bf7244
bshayer <124089539+bshayer-ba2007129@users.noreply.github.com>
2024-03-30 13:55:01 +0300

    latest update

---
+++ Phase1/js/history.js
@@ -0,0 +1,19 @@
+
+
+document.addEventListener('DOMContentLoaded', async () => {
+    try {
+        showHistory()
+        // other functions????
+    } catch (error) {
+      //  alert("you need to log in")
+    }
+});
+
+
+
+function showHistory() {
+      const customer = JSON.parse(localStorage.currentUser)
+      console.log(c);
+
+    
+}

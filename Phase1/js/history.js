eb8ae433cc3709e95fd0b955ec6a56dd49ce6e10
bshayer <124089539+bshayer-ba2007129@users.noreply.github.com>
2024-03-30 14:33:03 +0300

    latest

--- Phase1/js/history.js
+++ Phase1/js/history.js
@@ -1,19 +1,46 @@
 
+const  userHistory= document.querySelector('#user-info');
 
 document.addEventListener('DOMContentLoaded', async () => {
     try {
-        showHistory()
-        // other functions????
+        let customer = JSON.parse(localStorage.currentUser)
+        if(customer!=undefined){
+        showHistory()}
+        // other functions????   
+        else{    
+            alert("you need to log in")
+        }
+    
     } catch (error) {
-      //  alert("you need to log in")
+    
     }
 });
 
-
+let purchasedItemsList= []
 
 function showHistory() {
       const customer = JSON.parse(localStorage.currentUser)
-      console.log(c);
+      console.log(customer);
+      purchasedItemsList = customer.purchasedItems
+      console.log(purchasedItemsList);
+      const htmlProducts = purchasedItemsList.map(product1=> productHtml(product1) ).join("")
+      console.log(htmlProducts);
+      userHistory.innerHTML= htmlProducts
+    
+}
 
+
+function productHtml(product) {
+    return ` 
+    <img src="${product.product_image}" alt="product image">
+    <div class="productName-fav">
+        <p>${product.product_name}</p>
+        <button type="button" id="heart"><i class="fa-regular fa-heart"></i></button>
+    </div>
+    <div class="price-purchase">
+        <p>QR <span>${product.product_price}</span></p>
+        <a id="link" href="/sub-pages/cart.html" onclick="purchaseItem(${product.id})">More Information</a>
+    </div>
+`   
     
 }

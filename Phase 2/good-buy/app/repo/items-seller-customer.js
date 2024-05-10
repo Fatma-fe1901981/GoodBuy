0b7886e17d2490c2a1b51f6f95b981f59fa90773
Fatma Ali Elbakry Elbadrawy <fe1901981@qu.edu.qa>
2024-05-10 22:53:59 +0300

    another fix to json and prisma
    Co-Authored-By: bshayer <124089539+bshayer-ba2007129@users.noreply.github.com>
    Co-Authored-By: Ferial-fm1706475 <97462422+Ferial-fm1706475@users.noreply.github.com>

---
+++ Phase 2/good-buy/app/repo/items-seller-customer.js
@@ -0,0 +1,47 @@
+import { PrismaClient } from '@prisma/client'
+const prisma = new PrismaClient()
+
+class ItemsSellerCustomer {
+    async getItems() {
+        try {
+            return await prisma.items.findMany()
+        } catch (error) {
+            return { error: error.message }
+        }
+    }
+
+    async getSeller() {
+        try {
+            return await prisma.seller.findMany()
+        } catch (error) {
+            return { error: error.message }
+        }
+    }
+
+    async getCustomer() {
+        try {
+            return await prisma.customer.findMany()
+        } catch (error) {
+            return { error: error.message }
+        }
+    }
+
+    async createItem(data) {
+        try {
+            return await prisma.items.create({ data })
+        } catch (error) {
+            return { error: error.message }
+        }
+    }
+
+    async updateItem(data) {
+        try {
+            return await prisma.items.update({ where: { id: data.id }, data })
+        } catch (error) {
+            return { error: error.message }
+        }
+    }
+
+    
+
+}

05693171b0ab5b7bc11d0e01794ce84069b30632
Ferial-fm1706475 <97462422+Ferial-fm1706475@users.noreply.github.com>
2024-05-11 04:47:08 +0300

    get put delete in id route

---
+++ Phase 2/good-buy/app/api/[id]/route.js
@@ -0,0 +1,18 @@
+import repo from "../../repo/items-seller-customer";
+export async function GET(request, { params }) {
+  const productId = params.id;
+  const product = await repo.getProductById(productId);
+  return Response.json(product, { status: 200 });
+}
+
+export async function PUT(request, { params }) {
+  const productid = params.id;
+  const product = await request.json();
+  const updatedproduct = await repo.updateproduct(productid, product);
+  return Response.json(updatedproduct);
+}
+export async function DELETE(request, { params }) {
+  const { id } = params;
+  const product = await repo.deleteproduct(id);
+  return Response.json(product, { status: 200 });
+}
